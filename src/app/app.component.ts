bbbfcdd219275422944926ce22a9cae17c82bdc9
Ianel Tombozafy <ianell.s118@gmail.com>
2025-11-10 10:58:04 +0300

    feat: hideMainLayout on /checkin route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd, RouterLink } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { User } from './interfaces/user';
 import { AuthService } from './services/auth.service';
 import { HttpResult } from './types/httpResult';
 import { ToastComponent } from './components/toast/toast.component';
+import { filter } from 'rxjs/operators';
+import { AcmLogoComponent } from "./components/acm-logo/acm-logo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ToastComponent],
+  imports: [RouterOutlet, NavbarComponent, ToastComponent, AcmLogoComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -18,11 +20,25 @@ export class AppComponent {
   public user!: User;
   public userConnected: boolean = false;
   public toast: {show: boolean, message: string} = {show: false, message: ''};
+  public hideMainLayout: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {
     this.verifyToken();
+    this.checkRoute()
+  }
+
+  checkRoute() {
+    this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)
+      ).subscribe((event: NavigationEnd) => {
+        if (event.url === '/checkin') {
+          this.hideMainLayout = true;
+        } else {
+          this.hideMainLayout = false;
+        }
+      });
   }
 
   verifyToken() {

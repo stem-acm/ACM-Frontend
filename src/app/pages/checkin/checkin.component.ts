60575e90dccf04a34b64bd8ad3111ee50ae0f95c
Ianel Tombozafy <ianell.s118@gmail.com>
2025-11-10 10:57:24 +0300

    feat: create checkin component

---
+++ src/app/pages/checkin/checkin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkin',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkin.component.html',
+  styleUrl: './checkin.component.css'
+})
+export class CheckinComponent {
+
+}

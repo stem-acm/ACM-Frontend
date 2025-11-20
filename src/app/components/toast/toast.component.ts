295842a315652206ef770400d135c916f7662421
Aili Fida Aliotti Christino <aili.fida.ac@gmail.com>
2025-11-20 23:57:06 +0100

    chore: add Prettier configuration and ignore files, update Angular and ESLint settings, and enhance component structure

--- src/app/components/toast/toast.component.ts
+++ src/app/components/toast/toast.component.ts
@@ -9,6 +9,8 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ToastComponent {
   @Input() message!: string;
+  @Output('closed') emiterClose = new EventEmitter<boolean>();
+
   @Output() closed = new EventEmitter<boolean>();
 
   close() {

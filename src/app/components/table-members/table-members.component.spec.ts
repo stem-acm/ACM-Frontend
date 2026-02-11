9563e15f4f9347d8b773ccfdbbba9ea477f017f2
Ianel Tombozafy <ianell.s118@gmail.com>
2026-02-11 12:14:06 +0300

    feature: merge cards page functionalities into members page, remove cards link on nav menu

--- src/app/components/table-members/table-members.component.spec.ts
+++ src/app/components/table-members/table-members.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { TableMembersComponent } from './table-members.component';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('TableMembersComponent', () => {
   let component: TableMembersComponent;
@@ -8,11 +10,20 @@ describe('TableMembersComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TableMembersComponent],
+      imports: [TableMembersComponent, TranslateModule.forRoot()],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            params: of({}),
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TableMembersComponent);
     component = fixture.componentInstance;
+    component.data = []; // Initialize required input
     fixture.detectChanges();
   });
 

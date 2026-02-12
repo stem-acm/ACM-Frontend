0ba8659ed824f5e5006ba7e3e6ad8dde2751d4c3
Ianel Tombozafy <ianell.s118@gmail.com>
2026-02-12 10:23:08 +0300

    feat: Add a modal alert component and integrate it into the stepper to display 'already scanned' check-in errors instead of a toast.

---
+++ src/app/components/modal-alert/modal-alert.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-modal-alert',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './modal-alert.component.html',
+  styleUrls: ['./modal-alert.component.css']
+})
+export class ModalAlertComponent {
+  @Input() isVisible = false;
+  @Input() title = 'Alert';
+  @Input() message = '';
+  @Output() close = new EventEmitter<void>();
+
+  onClose() {
+    this.close.emit();
+  }
+}

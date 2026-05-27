28d8e8c09bffc9e2c20d038f59d3e53f47d36dae
Ianel Tombozafy <ianell.s118@gmail.com>
2026-05-27 12:23:35 +0300

    fix: change app environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,13 @@
-export const environment = {
+/*export const environment = {
   API_URL: 'https://acm-backend.lazygeko.app/api',
   FILE_URL: 'https://acm-backend.lazygeko.app/uploads',
   STATIC_WEB_URL: '',
   SHOW_EXPERIMENTAL_FEATURES: false,
+};
+*/
+export const environment = {
+  API_URL: 'http://localhost:5002/api',
+  FILE_URL: 'http://localhost:5002/uploads',
+  STATIC_WEB_URL: 'http://localhost:5002',
+  SHOW_EXPERIMENTAL_FEATURES: true,
 };

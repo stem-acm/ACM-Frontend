80d6a9241d068ecaf44ef724bf2b89c5bed43d14
Ianel Tombozafy <ianell.s118@gmail.com>
2026-05-30 12:29:06 +0300

    design: update table members styling

--- src/app/pages/members/members.component.ts
+++ src/app/pages/members/members.component.ts
@@ -38,7 +38,7 @@ export class MembersComponent implements OnInit {
   public searchWord = '';
 
   public currentPage = 1;
-  public pageSize = 10;
+  public pageSize = 200;
   public totalMembers = 0;
   public isLoading = false;
   private searchSubject = new Subject<string>();
